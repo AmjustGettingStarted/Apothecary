e929ec54efc4209d24bee8ee99309826dbd86d34
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-06-08 22:23:16 +0530

    fix(FAQ): dangerouslyHTML fix

--- app/page.js
+++ app/page.js
@@ -150,7 +150,7 @@ export default function Home() {
                       <AccordionContent>
                         {/* {benefit.answer} */}
                      
-                          <p
+                          <div
                             className="text-muted-foreground"
                             dangerouslySetInnerHTML={{ __html: benefit.answer }}
                           />
